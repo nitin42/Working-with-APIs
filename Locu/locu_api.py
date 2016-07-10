886deaa9dc0bf026f6cd4d79523e2e7860c12932
Nitin Tulswani <tulswani19@gmail.com>
2016-07-10 21:03:33 +0530

    Locu_search

---
+++ Locu/locu_api.py
@@ -0,0 +1,34 @@
+# Locu API
+
+import requests
+
+import json
+
+
+def search_restaurant(locality):
+	api_key = "bb5f92fb7f236891948704449f13f083ce2c1978"
+	api_endpoint = "http://api.locu.com/v1_0/venue/search/"
+	#locality = 'Delhi'
+	category = "restaurant"
+	api_url = api_endpoint + "?locality=" + str(locality) + "&category=" + category + "&api_key=" + api_key
+
+	res = requests.get(api_url)
+	data = json.loads(res.content)
+	for item in data["objects"]:
+		print (item["name"]) #+ " --> " + item["phone"])
+		print venue_details(item["id"])
+
+def venue_details(venue_id):
+	api_key = "bb5f92fb7f236891948704449f13f083ce2c1978"
+	api_endpoint = "http://api.locu.com/v1_0/venue/"
+	#locality = 'New%20York'
+	#category = "restaurant"
+	api_url = api_endpoint  + venue_id + "/" +"?api_key=" + api_key
+
+	res = requests.get(api_url)
+	data = res.json()
+	return data["objects"][0]["name"], data["objects"][0]["has_menu"], data["objects"][0]["phone"]
+
+
+if __name__ == "__main__":
+	search_restaurant(raw_input())	
