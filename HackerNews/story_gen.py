66921b1d4aacc118a54d157924c8c5b59d09420d
Nitin Tulswani <tulswani19@gmail.com>
2016-07-22 18:25:43 +0530

    Update story_gen.py

--- HackerNews/story_gen.py
+++ HackerNews/story_gen.py
@@ -1,17 +1,25 @@
-import requests
+'''Hacker-News API built on the top of firebaseio. To retreive the top stories, we generate a random id 
+because each story is associated with a unique id. Uncomment the second method and comment the first one if 
+you want to generate the news by specifying a particular id instead of randomly generating'''
 
+from random import randint
+
+import requests
 import json
 
-def hacker_news(news_id):
-	api_endpoint = 'https://hacker-news.firebaseio.com/v0/item/' + news_id + '.json?print=pretty' 
-	obj = requests.get(api_endpoint)
-	data = json.loads(obj.content)
-	for item in data:
-		print item[0] # By
-		print item[5] # score
-		print item[7] # title
-		print item[8] # type
-		print item[9] # url to the story
+api_endpoint = 'https://hacker-news.firebaseio.com/v0/item/' + str(randint(1,100000)) + '.json?print=pretty' 
+obj = requests.get(api_endpoint)
+res = obj.content
+data = json.loads(res)
+print data
 
-if __name__ == '__main__':
-	hacker_news(int(raw_input()))
+# def hacker_news(news_id):
+	# api_endpoint = 'https://hacker-news.firebaseio.com/v0/item/' + str(news_id) + '.json?print=pretty' 
+	# obj = requests.get(api_endpoint)
+	# res = obj.content
+	# data = json.loads(res)
+	# print data
+
+
+# if __name__ == '__main__':
+	# hacker_news(int(raw_input()))
